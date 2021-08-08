932dfdca23514b6db2df528213d9f7da2a49fccc
Wyatt <taebee93@gmail.com>
2021-08-08 15:43:04 +0900

    update[v2] train.py
    - coco evaluation 위한, AiHub class coco dataset 형식에 맞게 수정
    - coco evaluation 결과 epoch마다 출력
    
    - loss error 수정
    - train_valid_split 추가

--- train.py
+++ train.py
@@ -1,33 +1,33 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug  8 03:04:52 2021
-
-@author: taebe
-"""
-
 import torch      #pytorch
 import torch.nn as nn     #pytorch network
 from torch.utils.data import Dataset, DataLoader      #pytorch dataset
-from torch.utils.tensorboard import SummaryWriter     #tensorboard
+from tensorboardX import SummaryWriter     #tensorboard
 import torchvision      #torchvision
 import torch.optim as optim     #pytorch optimizer
 import numpy as np      #numpy
-import matplotlib.pyplot as plt     #matplotlib(이미지 표시를 위해 필요)
-from collections import OrderedDict     #python라이브러리 (라벨 dictionary를 만들 때 필요)
+
 import os     #os
+
 import cv2      #opencv (box 그리기를 할 때 필요)
 from PIL import Image     #PILLOW (이미지 읽기)
 import time     #time
 import imgaug as ia     #imgaug
 from imgaug import augmenters as iaa
 from torchvision import transforms      #torchvision transform
+
 import json as js
+from tqdm import tqdm
+from engine import evaluate
+import pickle
 
 #GPU연결
 if torch.cuda.is_available():
     device = torch.device('cuda:0')
 else:
     device = torch.device('cpu')
+#device=torch.device('cpu')
+
+
 print(device)
 
 ROOT_DIR = '../'
@@ -87,8 +87,9 @@ def json_parser(jfile):
         try:
             class_id = class_name_to_id_mapping[json_attr_detail]
         except KeyError:
-            print(json_attribute, json_attr_detail)
-            print("Invalid Class. Must be one from ", class_name_to_id_mapping.keys())
+            #print(json_attribute, json_attr_detail)
+            #print("Invalid Class. Must be one from ", class_name_to_id_mapping.keys())
+            pass
 
         json_bbox = list.get("points")
         one_bbox = []
@@ -117,7 +118,6 @@ def makeBox(voc_im, bbox, objects):
     return image
 
 def Total_Loss(loss):
-    # GPU 메모리를 위해 float 데이터만 저장한다.
     loss_objectness = loss['loss_objectness'].item()
     loss_rpn_box_reg = loss['loss_rpn_box_reg'].item()
     loss_classifier = loss['loss_classifier'].item()
@@ -131,6 +131,8 @@ def Total_Loss(loss):
     return total_loss
 
 
+global prev_img
+global prev_targets
 #
 #
 #
@@ -150,18 +152,23 @@ class AiHub(Dataset):
         jfile = str(self.json_list[idx])
 
         file_name, object_name, bbox = json_parser(jfile)
-
+        num_objs = len(object_name)
 
         image_path = IMG_DIR+str(file_name)+'.png'
         image = Image.open(image_path).convert("RGB")
         image = np.array(image)
         #print(file_name, ':', bbox)
-
+        image_id = torch.tensor([idx])
+        #
         image, bbox = self.flip(image=image, bounding_boxes=np.array([bbox]))
         image, bbox = self.resize(image=image, bounding_boxes=bbox)
-        bbox = bbox.squeeze(0).tolist()
         image = self.to_tensor(image)
 
+        area = (bbox[0][:, 3] - bbox[0][:, 1]) * (bbox[0][:, 2] - bbox[0][:, 0])
+        bbox = bbox.squeeze(0).tolist()
+
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+        masks = torch.zeros((num_objs,), dtype=torch.int64)
         targets = []
         d = {}
         d['boxes'] = torch.tensor(bbox, device=device)
@@ -169,8 +176,12 @@ class AiHub(Dataset):
             d['labels'] = torch.tensor([label_dic[x] for x in object_name], dtype=torch.int64, device=device)
         except:
             d['labels'] = torch.tensor([label_dic['세단'] for x in object_name], dtype=torch.int64, device=device)
-
+        d['image_id'] = image_id
+        d["masks"] = masks
+        d["area"] = area
+        d["iscrowd"] = iscrowd
         targets.append(d)
+
         return image, targets
 
 def run():
@@ -181,13 +192,10 @@ def run():
     label_set = set()
     cnt = 0
     tot_num_json = int(len(json_list))
-    for json in json_list:
+    for json in tqdm(range(len(json_list))):
         cnt += 1
-        file_name, object_name, bbox = json_parser(json)
-        print(f'{cnt} / {tot_num_json}')
-
-
-    dataset = AiHub(json_list, tot_num_json)
+        file_name, object_name, bbox = json_parser(json_list[json])
+        #print(f'{cnt} / {tot_num_json}')
 
     # #
     # #
@@ -234,14 +242,14 @@ def run():
     backbone = torchvision.models.mobilenet_v2(pretrained=True).features
     backbone.out_channels = 1280
   
-    num_classes = len(label_dic)  # 12 개 클래스
+    num_classes = len(label_dic)  # 13 개 클래스
     # get number of input features for the classifier
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     # replace the pre-trained head with a new one
     from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
   
-    model = torchvision.models.detection.FasterRCNN(backbone, num_classes=12)
+    model = torchvision.models.detection.FasterRCNN(backbone, num_classes=13)
   
     '''
     backbone = torchvision.models.vgg16(pretrained=True).features[:-1]
@@ -256,7 +264,7 @@ def run():
 
     box_head = torchvision.models.detection.faster_rcnn.TwoMLPHead(in_channels=backbone_out * (resolution ** 2),
                                                                    representation_size=4096)
-    box_predictor = torchvision.models.detection.faster_rcnn.FastRCNNPredictor(4096, 12)  # 12 class
+    box_predictor = torchvision.models.detection.faster_rcnn.FastRCNNPredictor(4096, 13)  # 13 class
 
     model = torchvision.models.detection.FasterRCNN(backbone, num_classes=None,
                                                     min_size=600, max_size=1000,
@@ -273,23 +281,6 @@ def run():
                                                     )
 
     loss_sum = 0
-    import utils
-    from engine import train_one_epoch, evaluate
-
-    #
-    # Train & Validation Split -> error
-    #
-    #from torch.utils.data.dataset import random_split
-    #lengths = [int(len(dataset) * 0.8), int(len(dataset) * 0.2)]
-    # train_dataset, val_dataset = random_split(dataset, [8571, 2142],
-    #                                           generator=torch.Generator().manual_seed(42))
-    # print(len(train_dataset), len(val_dataset))
-    # # define training and validation data loaders
-    # train_loader = torch.utils.data.DataLoader(
-    #     train_dataset, batch_size=2, shuffle=True)
-    #
-    # test_loader = torch.utils.data.DataLoader(
-    #     val_dataset, batch_size=1, shuffle=False)
 
     model.to(device)
     params = [p for p in model.parameters() if p.requires_grad]
@@ -299,10 +290,14 @@ def run():
     #
     # training
     #
-    len_data = len(dataset)  # voc2007은 5011개 이미지
+    total_epoch = 10 # 1번에 1시간 소요
+
+    writer = SummaryWriter("../models/faster_rcnn/runs/faster_rcnn")
+    #f_evaluation = open("coco_eval.txt", 'w')
+
     term = 10
     try:
-        check_point = torch.load(ROOT_DIR + "Models/Faster_RCNN/Check_point.pth")
+        check_point = torch.load(ROOT_DIR + "models/faster_rcnn/check_point.pth")
         start_epoch = check_point['epoch']
         start_idx = check_point['iter']
         model.load_state_dict(check_point['state_dict'])
@@ -311,9 +306,6 @@ def run():
                                                                last_epoch=start_epoch)
         scheduler.load_state_dict(check_point['scheduler'])
 
-        if start_idx == len_data:
-            start_idx = 0
-            start_epoch = start_epoch + 1
 
     except:
         print("check point load error!")
@@ -321,46 +313,52 @@ def run():
         start_idx = 0
 
 
-    total_epoch = 10
 
     # print("start_epoch = {} , start_idx = {}".format(start_epoch, start_idx))
     print("Training Start")
 
     model.train()
-    print("pass")
-
-    gc.collect()
-    torch.cuda.empty_cache()
 
     start = time.time()
-    dataloader = DataLoader(dataset, shuffle=True, batch_size=1)
+    #
+    # 이부분 training / validation 으로 나누기
+    #
+    from sklearn.model_selection import train_test_split
+
+    train_json_list, valid_json_list = train_test_split(json_list, test_size=0.3, random_state=42)
+    train_dataset = AiHub(train_json_list, len(train_json_list))
+    valid_dataset = AiHub(valid_json_list, len(valid_json_list))
+
+    dataloader = DataLoader(train_dataset, shuffle=True, batch_size=1)
+    validloader = DataLoader(valid_dataset, shuffle=False, batch_size=1)
+
+    print(f'train:valid = {len(dataloader)}:{len(validloader)}')
+
     for epoch in range(start_epoch, total_epoch):
 
-        # writer.add_scalar('Learning Rate', scheduler.get_last_lr()[0], epoch)
+        writer.add_scalar('Learning Rate', scheduler.get_last_lr()[0], epoch)
 
-        #
-        # 이부분 training / validation 으로 나누기 -> error
-        #
-        #dataset = AiHub(json_list[:len_data], len_data - start_idx)
-
-        failed = []
-        #print(len(dataloader))
-        #try:
         for i, (image, targets) in enumerate(dataloader, start_idx):
             optimizer.zero_grad()
-            # print(i, image, targets) #####
+
             targets[0]['boxes'].squeeze_(0)
             targets[0]['labels'].squeeze_(0)
+            try:
+                loss = model(image.to(device), targets)
+            except:
+                print('loss error!')
+                continue
 
-            loss = model(image.to(device), targets)
+            #print("loss= ",loss)
             total_loss = Total_Loss(loss)
             loss_sum += total_loss
-
             if (i + 1) % term == 0:
                 end = time.time()
-                print("Epoch {} | Iter {}/{} | Loss: {} | Duration: {} sec".format(epoch, (i + 1), len(dataloader),
-                                                                                   (loss_sum / term), int((end - start))))
-                # writer.add_scalar('Training Loss', loss_sum / term, epoch * len_data + i)
+                print("Epoch {}/{} | Iter {}/{} | Loss: {} | Duration: {} sec".format(epoch, total_epoch,
+                                                                                      (i + 1), len(dataloader),
+                                                                                      (loss_sum / term),
+                                                                                      round((end - start), 3)))
+                writer.add_scalar('Training Loss', loss_sum / term, epoch * len(dataloader) + i)
 
                 state = {
                     'epoch': epoch,
@@ -369,21 +367,15 @@ def run():
                     'optimizer': optimizer.state_dict(),
                     'scheduler': scheduler.state_dict()
                 }
-                torch.save(state, ROOT_DIR + "Models/Faster_RCNN/Check_point.pth")
+                torch.save(state, ROOT_DIR + "models/faster_rcnn/check_point.pth")
 
                 loss_sum = 0
                 start = time.time()
-        #except:
-        #    print('tick')
-        #    continue
 
-        total_loss.backward()
+        #total_loss.backward()
         optimizer.step()
-        print('pass-opti')
         start_idx = 0
         scheduler.step()
-        print('pass-lr')
-        #evaluate(model, test_loader, device=device)
 
         state = {
             'epoch': epoch,
@@ -392,55 +384,64 @@ def run():
             'optimizer': optimizer.state_dict(),
             'scheduler': scheduler.state_dict()
         }
-        torch.save(state, ROOT_DIR + "Models/Faster_RCNN/Check_point.pth")
+        torch.save(state, ROOT_DIR + "models/faster_rcnn/check_point.pth")
+
+        #
+        # evaluation
+        #
+        model.eval()
+        with torch.no_grad():
+            coco_eval = evaluate(model, validloader, device=device)
+            with open('path_and_filename.pickle', 'wb') as handle:
+                pickle.dump(coco_eval.coco_eval, handle)
+
+        model.train()
 
         if (epoch + 1) % 2 == 0:
-            torch.save(model.state_dict(), ROOT_DIR + "Models/Faster_RCNN/Epoch{}.pth".format(epoch))
+            torch.save(model.state_dict(), ROOT_DIR + "models/faster_rcnn/epoch{}.pth".format(epoch))
 
-NULL_CNT = 0
-def preprocessing(root_dir):
-    files = os.listdir(root_dir)
-    k = 0
-    tot_num = len(files)
-    tot = []
-    for file in files:
-        print_buffer = []
-        path = os.path.join(root_dir, file)
-        file_name = path
-        # print(file_name.split('\\')[4].split('.')[0])
-        #print(path)
-        # JSON 파일읽기
-        with open(path, encoding='UTF8') as json_file:
-            json_data = js.load(json_file)
-        # JSON 내 annotation 배열 설정
-        jsonArray = json_data.get("annotations")
-        # annotation 배열 열고 읽기
-        k += 1
-        #print(f'{k}/{tot_num}')
-        for list in jsonArray:
-            json_label = list.get("label")
-            json_attribute = list.get("attributes")
-            json_attr_detail = json_attribute.get(json_label)
-            if list.get('annotation') == {}:
-                print(file_name)
-                break
-            try:
-                class_id = class_name_to_id_mapping[json_attr_detail]
-            except KeyError:
-                print(json_attribute, json_attr_detail)
-                print(file_name)
-                print("Invalid Class. Must be one from ", class_name_to_id_mapping.keys())
+    writer.close()
+    #f_evaluation.close()
 
-            # json_bbox = list.get("points")
-            # np_bbox = np.array(json_bbox)
-            # if len(np_bbox) == 0:
-            #     print("List x is Empty")
+def f1_loss(y_true: torch.Tensor, y_pred: torch.Tensor, is_training=False) -> torch.Tensor:
+    '''Calculate F1 score. Can work with gpu tensors
+
+    The original implmentation is written by Michal Haltuf on Kaggle.
+
+    Returns
+    -------
+    torch.Tensor
+        `ndim` == 1. 0 <= val <= 1
+
+    Reference
+    ---------
+    - https://www.kaggle.com/rejpalcz/best-loss-function-for-f1-score-metric
+    - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html#sklearn.metrics.f1_score
+    - https://discuss.pytorch.org/t/calculating-precision-recall-and-f1-score-in-case-of-multi-label-classification/28265/6
+
+    '''
+    assert y_true.ndim == 1
+    assert y_pred.ndim == 1 or y_pred.ndim == 2
+
+    if y_pred.ndim == 2:
+        y_pred = y_pred.argmax(dim=1)
+
+    tp = (y_true * y_pred).sum().to(torch.float32)
+    tn = ((1 - y_true) * (1 - y_pred)).sum().to(torch.float32)
+    fp = ((1 - y_true) * y_pred).sum().to(torch.float32)
+    fn = (y_true * (1 - y_pred)).sum().to(torch.float32)
+
+    epsilon = 1e-7
+
+    precision = tp / (tp + fp + epsilon)
+    recall = tp / (tp + fn + epsilon)
+
+    f1 = 2 * (precision * recall) / (precision + recall + epsilon)
+    f1.requires_grad = is_training
+    return f1
 
 
 if __name__ == '__main__':
-    #preprocessing(JSON_DIR)
+
     run()
-'''
 
-
-'''
